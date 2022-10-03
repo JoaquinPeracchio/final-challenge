3fcfdcb946f1db558d656010a4f0b73f02a53a25
jojope38 <peracchiojoaquin16@gmail.com>
2022-10-03 14:10:09 -0300

    add page welcome , signs and folder features

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignUp from '../components/SignUp'
+import SignUp from '../components/Sign-up/SignUp'
 export default function signUp() {
   return (
     <signUp></signUp>
