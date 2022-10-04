9eef0f575f5deb43b6c3ed5007eba6a1e6342bff
LeonardoMedici <leomedici06@gmail.com>
2022-10-04 16:26:16 -0300

    Instalacion de boostrap y uso

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import CarouselBootstrap from '../components/CarouselBootstrap/CarouselBootstrap'
 export default function Welcome() {
   return (
-    <Hero></Hero>
+    <div>
+      <Hero></Hero>
+      <CarouselBootstrap />
+    </div>
   )
 }
