ea54529979a1c769a20132ae43aa3c865e66a7e6
LeonardoMedici <leomedici06@gmail.com>
2022-10-06 18:43:18 -0300

    Boton hamburguesa para sign

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import {Link as LinkRouter} from 'react-router-dom'
 
 export default function Hero() {
   return (
-    <header className="header" Style="">
+    <header className="header">
       <img className='logo' src="" alt="MyTinerary logo" />
       <h1 className='hero-title'>MyTinerary</h1>
       <LinkRouter to="/home" className="header-btn">ENTER</LinkRouter>
--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -8,36 +8,42 @@ export const Productos = () => {
         {
             img: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
             title: 'titulo1',
-            description: 'description1'
+            description: 'description1',
+            alt: 'producto1'
         },
         {
             img: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
             title: 'titulo2',
-            description: 'description2'
+            description: 'description2',
+            alt: 'producto2'
         },
         {
             img: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
             title: 'titulo3',
-            description: 'description3'
+            description: 'description3',
+            alt: 'producto3'
         }, {
             img: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
             title: 'titulo1',
-            description: 'description1'
+            description: 'description1',
+            alt: 'producto4'
         },
         {
             img: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
             title: 'titulo2',
-            description: 'description2'
+            description: 'description2',
+            alt: 'producto5'
         },
         {
             img: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
             title: 'titulo3',
-            description: 'description3'
+            description: 'description3',
+            alt: 'producto6'
         },
     ]
 
     const cardBootstrap = (items) =>
-        <Card className="card-product" style={{ width: '18rem' }}>
+        <Card key={items.alt} className="card-product" style={{ width: '18rem' }}>
             <Card.Img className="imgCarousel" src={items.img} />
             <Card.Body>
                 <Card.Title>{items.title}</Card.Title>
