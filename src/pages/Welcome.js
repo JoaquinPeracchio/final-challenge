638fce1beea5819d21f292b53394d62dc3077c0c
LeonardoMedici <leomedici06@gmail.com>
2022-10-09 22:21:12 -0300

    Agregado de boton carrito

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import CarouselBootstrap from '../components/CarouselBootstrap/CarouselBootstrap'
-import { Productos } from '../components/Productos/Productos'
 import '../components/Productos/Productos.css'
 
 export default function Welcome() {
   return (
     <div className='welcome-container'>
-      <div>
         <Hero></Hero>
         <CarouselBootstrap />
-      </div>
-      <Productos />
     </div>
   )
 }
