300a328e781a94cfef3967fc7ff12f1c190ba31f
LautiAlessa <alessandrolautaror@gmail.com>
2022-10-10 17:45:45 -0300

    agrego linkrouter a prductos al boton enter

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link as LinkRouter} from 'react-router-dom'
+import '../Productos/Productos.css'
 
 export default function Hero() {
   return (
     <header className="header">
       <img className='logo' src="" alt="MyTinerary logo" />
       <h1 className='hero-title'>MyTinerary</h1>
-      <LinkRouter to="/home" className="header-btn">ENTER</LinkRouter>
+      <LinkRouter to="/products" className="header-btn">ENTER</LinkRouter>
     </header>
   );
 }
