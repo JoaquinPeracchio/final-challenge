2ccc8889f5f7db66d05e6263b6fab5611555bf70
LeonardoMedici <leomedici06@gmail.com>
2022-10-11 18:43:18 -0300

    Estilos de cartas

--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -6,87 +6,97 @@ import { AddCarrito } from '../../features/slices/carritoSlice';
 import './Productos.css'
 
 export const Productos = () => {
-    const [state,setState]=useState(false)
-    const [product , setProduct]=useState({})
+    const [state, setState] = useState(false)
+    const [product, setProduct] = useState({})
     const sendInfo = useDispatch()
 
     const productos = [
         {
-            img: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
+            photo: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
             title: 'titulo1',
             description: 'description1',
-            quantity:1,
-            alt: 'producto1'
+            quantity: 1,
+            alt: 'producto1',
+            price: 123,
+            name: 'productName'
         },
         {
-            img: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
+            photo: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
             title: 'titulo2',
             description: 'description2',
-            quantity:1,
-            alt: 'producto2'
+            quantity: 1,
+            alt: 'producto2',
+            price: 123,
+            name: 'productName'
         },
         {
-            img: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
+            photo: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
             title: 'titulo3',
             description: 'description3',
-            quantity:1,
-            alt: 'producto3'
+            quantity: 1,
+            alt: 'producto3',
+            price: 123,
+            name: 'productName'
         }, {
-            img: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
+            photo: 'https://www.zilliondesigns.com/blog/wp-content/uploads/Cat-logos.png',
             title: 'titulo1',
             description: 'description1',
-            quantity:1,
-            alt: 'producto4'
+            quantity: 1,
+            alt: 'producto4',
+            price: 123,
+            name: 'productName'
         },
         {
-            img: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
+            photo: 'https://static.vecteezy.com/system/resources/previews/006/862/726/original/icon-of-a-cat-animal-logo-with-an-adorable-pose-free-vector.jpg',
             title: 'titulo2',
             description: 'description2',
-            quantity:1,
-            alt: 'producto5'
+            quantity: 1,
+            alt: 'producto5',
+            price: 123,
+            name: 'productName'
         },
         {
-            img: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
+            photo: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/1631/posts/38685/image-upload/CatLogoDesign003.jpg',
             title: 'titulo3',
             description: 'description3',
-            quantity:1,
-            alt: 'producto6'
+            quantity: 1,
+            alt: 'producto6',
+            price: 123,
+            name: 'productName'
         },
     ]
 
-    const handleClose =()=>{
+    const handleClose = () => {
         setState(false)
     }
 
-    const showDetails =(e)=>{
+    const showDetails = (e) => {
         setProduct(e)
         setState(true)
     }
 
-    const setCarrito = (e)=>{
+    const setCarrito = (e) => {
         sendInfo(AddCarrito(e))
 
     }
 
     const cardBootstrap = (items) =>
-        <Card key={items.alt} className="card-product" style={{ width: '18rem' }}>
-            <button style={{border:'none' , cursor:'pointer'}} onClick={()=>showDetails({image:items.img,title:items.title,description:items.description})}>
-            <Card.Img className="imgCarousel" src={items.img} />
-             </button>
-            <Card.Body>
-                <Card.Title>{items.title}</Card.Title>
-                <Card.Text>
-                    {items.description}
-                </Card.Text>
-            </Card.Body>
-            <button onClick={()=>setCarrito({image:items.img,title:items.title,description:items.description,quantity:items.quantity})} style={{border:'none', cursor:'pointer'}}>Add carrito</button>
-        </Card>
+        <div key={items.alt} className="card-product" style={{ width: '18rem' }}>
+            <button className='btn-details' style={{ border: 'none', cursor: 'pointer' }} onClick={() => showDetails({ image: items.photo, title: items.title, description: items.description })}>
+                <Card.Img className="imgCarousel" src={items.photo} />
+            </button>
+            <div className='body-card-container'>
+                <div className='name-product'>{items.name}</div>
+                <div className='card-body'> ${items.price}.00</div>
+                <button className='add-carrito-btn' onClick={() => setCarrito({ image: items.photo, title: items.title, description: items.description, quantity: items.quantity })} style={{ border: 'none', cursor: 'pointer' }}>Add to cart</button>
+            </div>
+        </div>
 
     return (
         <div>
             <input className="product-filter" type="text" placeholder="Search product"></input>
             <div className="producs-container">
-                {!state?productos.map(cardBootstrap):<Details props={product} onclose={handleClose}/>}
+                {!state ? productos.map(cardBootstrap) : <Details props={product} onclose={handleClose} />}
             </div>
         </div>
     )
