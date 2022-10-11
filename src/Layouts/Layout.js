ae7f57ff63b44770914c690e452752c889248034
LilAguh <aguezeochoa@gmail.com>
2022-10-11 13:33:48 -0300

    added footer in layout

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -8,7 +8,7 @@ export default function Layout(props) {
         <>
             <Header />
             {props.children}
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
