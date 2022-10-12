007b28892eb0a21c4fa73e30f1b78db31cc76c84
Joaquin Peracchio <88782590+JoaquinPeracchio@users.noreply.github.com>
2022-10-12 13:23:34 -0300

    Update carritoSlice.js

--- src/features/slices/carritoSlice.js
+++ src/features/slices/carritoSlice.js
@@ -6,41 +6,42 @@ const initialState = [
 ]
 
 export const carritoSlice = createSlice({
-    name: 'carrito',
+    name:'carrito',
     initialState,
-
     reducers:{
         AddCarrito : (state,action) =>{ 
-            const repeat =   state.find(item => item.title === action.payload.title)  
+            const repeat =   state.find(item => item.id === action.payload.id)  
                if(repeat){
                 repeat.quantity += 1
                }else{
                    state.push(action.payload)          
 
                }
+        },
+        DeleteProduct : (state,action) =>{
+         const remove =   state.find(item => item.id === action.payload.id)
+         state.pop(remove)
 
         },
-        DeleteProduct: (state, action) => {
-            const remove = state.find(item => item.id === action.payload.id)
-            state.pop(remove)
+        UpdateCarrito : (state,action)=>{
+            const update = state.find(item => item.id ===action.payload.id)
+            if(action.payload.quantity!=0){
+                update.quantity = action.payload.quantity
+               
 
+            }
         },
-        UpdateCarrito: (state, action) => {
-            const update = state.find(item => item.id === action.payload.id)
-            update.quantity = action.payload.quantity
-            update.price = update.price * action.payload.quantity
-        },
-
-        ClearCarrito: (state) => {
-            [...state] === []
-
+        ClearCarrito : (state) =>{
+          
         }
 
     }
 })
 
-export const { AddCarrito, DeleteProduct } = carritoSlice.actions
+export const { AddCarrito,DeleteProduct,UpdateCarrito} = carritoSlice.actions
 export default carritoSlice.reducer
+
+
 
 
 
