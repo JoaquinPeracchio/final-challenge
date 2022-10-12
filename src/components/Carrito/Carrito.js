44c329031e16e1487dd1aed324f3bde90ed98366
jojope38 <peracchiojoaquin16@gmail.com>
2022-10-12 12:27:55 -0300

    carrito completed

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { DeleteProduct } from '../../features/slices/carritoSlice'
+import { DeleteProduct , UpdateCarrito  } from '../../features/slices/carritoSlice'
+
 export default function Carrito({onclose}) {
 
 
 const [price,setPrice]=useState(0)
-
+const counter = 0
 
 
 const currentCarrito = useSelector(state => state.carrito)
+console.log(currentCarrito)
 const sendInfo = useDispatch()
 
 
 
-const saveElement =()=>{
-  localStorage.setItem('carrito',JSON.stringify(currentCarrito))
+// const saveElement =()=>{
+//   localStorage.setItem('carrito',JSON.stringify(currentCarrito))
+// }
+
+ const clearElement = (e)=>{
+  setPrice(price+e)
 }
-
-const clearElement = ()=>{
-
-}
-
 
 
 const handleSubmit = (e)=>{
@@ -37,50 +38,49 @@ const handleSubmit = (e)=>{
 
 }
 const removeElem =(e)=>{
+  console.log(e)
   sendInfo(DeleteProduct(e))
 }
-/*data del modelo product{
-  foto
-  product
-  type
-  cantidad seteada por kg
-  la cantidad de esa cantaidad que va a llevar 
-  stock
-  state (bueno muy bueno malo muy malo)
-  */
 
 
+let priceArr=[]
+console.log(priceArr)
 let showCarrito =(item)=>(
-  setPrice(price + item.price),
-  item.variant=== fruit?
-    <div>
-      <img src={item.image}></img>
-      <h1>Product:{item.title}</h1>
-      <p>Variant: {item.description}</p>
-      {/* price : item.price */}
-      {/* cantidad id=item.id onclick(()=>handleCantidad({cantidad :e.target.value,id:e.target.id})) : item.price */}
-    <button value={item.id}  onClick={e=>removeElem(e.target.value)}>X</button>
-    </div>
-  :
-  <div>
-  <img src={item.photo}></img>
-  <h1>Product:{item.product}</h1>
-  <p>Variant: {item.variant}</p>
-  <p>Current State: {item.state}</p>
-  <button value={item.id}  onClick={e=>removeElem(e.target.value)}>X</button>
+
+  
+  <div onLoad={()=>clearElement(item.price)}>
+  <button value={item.id}  onClick={(e)=>removeElem(e.target.value)}>X</button>
+ <img src={item.image}></img>
+  <h1>Product:{item.name}</h1>
+  <p>Variety: {item.variety}</p>
+  <p>{item.description}</p>
+  <p>Current State: {item.currentState}</p>
+  <p>Price : ${item.price * item.quantity}</p>
+  <p> KG: {item.quantitymin}</p>
+  <p> quantity {item.quantity}</p>
+  
+  <p>#
+  {priceArr.push({
+    idProd : item.id,
+    name:item.name,
+    image:item.image,
+    quantity:item.quantity,
+    price: item.price * item.quantity
+  })}</p>
 </div>
+
 
   
 )
 
-  
+
   return (
     <div>
-      <button onClick={onclose}></button>
-      {show?currentCarrito?currentCarrito.map(e => showCarrito(e) ):'':''}
-      <h3>Total de Compra: {price}</h3>
-      <button onClick={saveElement}>save</button>
-      <button onClick={clearElement}>Clear</button>
+      <button onClick={onclose}>X</button>
+      {currentCarrito.map( showCarrito )}
+      <h3>Total de Compra: {priceArr.map(item => item.price).reduce((prev, curr) => prev + curr, 0)}</h3>
+      {/* <button onClick={saveElement}>save</button>
+      <button onClick={clearElement}>Clear</button> */}
       <button onClick={handleSubmit}>Finalizar Compra</button>
     </div>
   )
