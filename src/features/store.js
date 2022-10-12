03c934507327bd4943907fa3d4bb5584bd0004c6
LilAguh <aguezeochoa@gmail.com>
2022-10-12 10:04:23 -0300

    endpoints are added in the store

--- src/features/store.js
+++ src/features/store.js
@@ -2,16 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import commentsReducer from './slices/commentsSlice'
 import carritoSlice from './slices/carritoSlice'
 import commentsApi from './actions/commentsApi'
-// import ApiMethod from './actions/ApiMethod'
+import userApi from './actions/userApi'
 
 export const store = configureStore({
   reducer: {
+    [userApi.reducerPath]: userApi.reducer,
     [commentsApi.reducerPath]: commentsApi.reducer,
     comments: commentsReducer,
     carrito: carritoSlice
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(commentsApi.middleware),
+    getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(commentsApi.middleware)
 })
 
