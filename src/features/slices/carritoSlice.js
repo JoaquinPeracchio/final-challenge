80c48b53c1d746ecf70bbdb1d5e680d54641b52b
LilAguh <aguezeochoa@gmail.com>
2022-10-13 15:34:00 -0300

    the cart bug when removing products is fixed

--- src/features/slices/carritoSlice.js
+++ src/features/slices/carritoSlice.js
@@ -6,39 +6,41 @@ const initialState = [
 ]
 
 export const carritoSlice = createSlice({
-    name:'carrito',
+    name: 'carrito',
     initialState,
-    reducers:{
-        AddCarrito : (state,action) =>{ 
-            const repeat =   state.find(item => item.id === action.payload.id)  
-               if(repeat){
+    reducers: {
+        AddCarrito: (state, action) => {
+            const repeat = state.find(item => item.id === action.payload.id)
+            if (repeat) {
                 repeat.quantity += 1
-               }else{
-                   state.push(action.payload)          
-
-               }
-        },
-        DeleteProduct : (state,action) =>{
-         const remove =   state.find(item => item.id === action.payload.id)
-         state.pop(remove)
-
-        },
-        UpdateCarrito : (state,action)=>{
-            const update = state.find(item => item.id ===action.payload.id)
-            if(action.payload.quantity!=0){
-                update.quantity = action.payload.quantity
-               
+            } else {
+                state.push(action.payload)
 
             }
         },
-        ClearCarrito : (state) =>{
-          
+        DeleteProduct: (state, action) => {
+            const remove = state.find(item => item.id === action.payload)
+            if (remove) {
+                state.splice(state.indexOf(remove), 1)
+            }
+
+        },
+        UpdateCarrito: (state, action) => {
+            const update = state.find(item => item.id === action.payload.id)
+            if (action.payload.quantity != 0) {
+                update.quantity = action.payload.quantity
+
+
+            }
+        },
+        ClearCarrito: (state) => {
+
         }
 
     }
 })
 
-export const { AddCarrito,DeleteProduct,UpdateCarrito} = carritoSlice.actions
+export const { AddCarrito, DeleteProduct, UpdateCarrito } = carritoSlice.actions
 export default carritoSlice.reducer
 
 
