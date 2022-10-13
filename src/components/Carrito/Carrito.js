fa7fae8e035fe6d569cf97c47f2c4dd89845f798
LeonardoMedici <leomedici06@gmail.com>
2022-10-12 21:34:49 -0300

    Estilos cart

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -1,87 +1,83 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { DeleteProduct , UpdateCarrito  } from '../../features/slices/carritoSlice'
+import { DeleteProduct, UpdateCarrito } from '../../features/slices/carritoSlice'
+import './Carrito.css'
 
-export default function Carrito({onclose}) {
+export default function Carrito({ onclose }) {
 
+  const [price, setPrice] = useState(0)
 
-const [price,setPrice]=useState(0)
+  const currentCarrito = useSelector(state => state.carrito)
+  console.log(currentCarrito)
+  const sendInfo = useDispatch()
 
+  // const saveElement =()=>{
+  //   localStorage.setItem('carrito',JSON.stringify(currentCarrito))
+  // }
 
-
-const currentCarrito = useSelector(state => state.carrito)
-console.log(currentCarrito)
-const sendInfo = useDispatch()
-
-
-
-// const saveElement =()=>{
-//   localStorage.setItem('carrito',JSON.stringify(currentCarrito))
-// }
-
- const clearElement = (e)=>{
-  setPrice(price+e)
-}
-
-
-const handleSubmit = (e)=>{
-  e.preventDefault()
-
-  if(currentCarrito.length != null){
-    //mutationsell(stock)
-    //mutationbuy(stock)
-
-  }else{
-    alert('your carrito its empty')
+  const clearElement = (e) => {
+    setPrice(price + e)
   }
 
-}
-const removeElem =(e)=>{
-  console.log(e)
-  sendInfo(DeleteProduct(e))
-}
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
+    if (currentCarrito.length != null) {
+      //mutationsell(stock)
+      //mutationbuy(stock)
 
-let priceArr=[]
-console.log(priceArr)
-let showCarrito =(item)=>(
+    } else {
+      alert('your carrito its empty')
+    }
 
-  
-  <div onLoad={()=>clearElement(item.price)}>
-  <button value={item.id}  onClick={(e)=>removeElem(e.target.value)}>X</button>
- <img src={item.image}></img>
-  <h1>Product:{item.name}</h1>
-  <p>Variety: {item.variety}</p>
-  <p>{item.description}</p>
-  <p>Current State: {item.currentState}</p>
-  <p>Price : ${item.price * item.quantity}</p>
-  <p> KG: {item.quantitymin}</p>
-  <p> quantity {item.quantity}</p>
-  
-  <p>#
-  {priceArr.push({
-    idProd : item.id,
-    name:item.name,
-    image:item.image,
-    quantity:item.quantity,
-    price: item.price * item.quantity
-  })}</p>
-</div>
+  }
+  const removeElem = (e) => {
+    console.log(e)
+    sendInfo(DeleteProduct(e))
+  }
 
+  let priceArr = []
+  console.log(priceArr)
+  let showCarrito = (item) => (
 
-  
-)
+    <div onLoad={() => clearElement(item.price)}>
+      <button className='quit-item' value={item.id} onClick={(e) => removeElem(e.target.value)}>X</button>
+      <div className='card-cart'>
+        <div className='image-cart-container'>
+          <img className="cart-image" src={item.image}></img>
+        </div>
+        <div className='cart-details'>
+          <h1 className='peso-letra'>Product:{item.name}</h1>
+          <p className='peso-letra fondo'>Variety: {item.variety}</p>
+          <p className='peso-letra fondo'>{item.description}</p>
+          <p className='peso-letra fondo'>Current State: {item.currentState}</p>
+          <p className='peso-letra fondo'>Price : ${item.price * item.quantity}</p>
+          <p className='peso-letra fondo'> KG: {item.quantitymin}</p>
+          <p className='peso-letra fondo'> quantity {item.quantity}</p>
+        </div>
+      </div>
 
+      <p>#
+        {priceArr.push({
+          idProd: item.id,
+          name: item.name,
+          image: item.image,
+          quantity: item.quantity,
+          price: item.price * item.quantity
+        })}</p>
+    </div>
+  )
 
   return (
-    <div>
-      <button onClick={onclose}>X</button>
-      {currentCarrito.map( showCarrito )}
-      <h3>Total de Compra: {priceArr.map(item => item.price).reduce((prev, curr) => prev + curr, 0)}</h3>
-      {/* <button onClick={saveElement}>save</button>
+    <div className='cart-page'>
+      <div className='cart-container'>
+        {currentCarrito.map(showCarrito)}
+        <h3>Total de Compra: {priceArr.map(item => item.price).reduce((prev, curr) => prev + curr, 0)}</h3>
+        {/* <button onClick={saveElement}>save</button>
       <button onClick={clearElement}>Clear</button> */}
-      <button onClick={handleSubmit}>Finalizar Compra</button>
+        <button className='finalizar-compra' onClick={handleSubmit}>Finalizar Compra</button>
+      </div>
     </div>
   )
 }
